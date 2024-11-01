609e351686c38c1b6be5ea3a95a26810128b3a49
edumarrero <edumarreroglezz@gmail.com>
2024-11-01 19:35:18 +0100

    History paneL UI improved!

--- src/main/java/software/ulpgc/apps/swing/SwingFavoritiesContent.java
+++ src/main/java/software/ulpgc/apps/swing/SwingFavoritiesContent.java
@@ -3,6 +3,7 @@ package software.ulpgc.apps.swing;
 import software.ulpgc.view.VisualComponent;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class SwingFavoritiesContent extends JPanel implements VisualComponent {
@@ -11,8 +12,15 @@ public class SwingFavoritiesContent extends JPanel implements VisualComponent {
         this.setLayout(new BorderLayout());
         this.setBackground(new Color(17, 21, 24));
 
-        JLabel labelPrueba = new JLabel("JUAN");
-        this.add(labelPrueba);
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        topPanel.setOpaque(false);
+        topPanel.setBorder(new EmptyBorder(20,0,0,0));
+
+        JLabel tittleLabel = new JLabel("Favorities");
+        tittleLabel.setFont(new Font("Arial Black", Font.BOLD, 20));
+        tittleLabel.setForeground(Color.WHITE);
+        topPanel.add(tittleLabel);
+        this.add(topPanel, BorderLayout.NORTH);
     }
 
     @Override
--- src/main/java/software/ulpgc/apps/swing/SwingHistoryContent.java
+++ src/main/java/software/ulpgc/apps/swing/SwingHistoryContent.java
@@ -5,6 +5,7 @@ import software.ulpgc.model.ExchangeTransaction;
 import software.ulpgc.view.VisualComponent;
 import java.util.List;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class SwingHistoryContent extends JPanel implements VisualComponent {
@@ -12,6 +13,16 @@ public class SwingHistoryContent extends JPanel implements VisualComponent {
     public SwingHistoryContent(ExchangeRecord exchangeRecord) {
         this.setLayout(new BorderLayout());
         this.setBackground(new Color(17, 21, 24));
+
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        topPanel.setOpaque(false);
+        topPanel.setBorder(new EmptyBorder(20,0,0,0));
+
+        JLabel tittleLabel = new JLabel("History");
+        tittleLabel.setFont(new Font("Arial Black", Font.BOLD, 20));
+        tittleLabel.setForeground(Color.WHITE);
+        topPanel.add(tittleLabel);
+        this.add(topPanel, BorderLayout.NORTH);
 
         JTable table = getTable(exchangeRecord.showHistory());
         table.setBackground(new Color(30, 30, 30));
@@ -26,7 +37,10 @@ public class SwingHistoryContent extends JPanel implements VisualComponent {
         table.getTableHeader().setForeground(Color.WHITE);
 
         JScrollPane scrollPane = new JScrollPane(table);
-        this.add(table, BorderLayout.CENTER);
+        scrollPane.setOpaque(true);
+        scrollPane.getViewport().setBackground(new Color(17, 21, 24));
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        this.add(scrollPane, BorderLayout.CENTER);
     }
 
     private JTable getTable(List<ExchangeTransaction> transactions){
